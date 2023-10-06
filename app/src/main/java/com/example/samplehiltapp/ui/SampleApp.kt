b0e026199da56fd6d599cc31d8fd799cd5c1afb6
Vaseeharan Srikandan <vaseeharansrikandan@Vaseeharans-MacBook-Pro-2.local>
2023-10-06 23:51:54 +0100

    Added git workflow for unit test and implemented compose

---
+++ app/src/main/java/com/example/samplehiltapp/ui/SampleApp.kt
@@ -0,0 +1,25 @@
+package com.example.samplehiltapp.ui
+
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SampleApp(modifier: Modifier = Modifier) {
+
+    var shouldShowOnboarding by rememberSaveable {
+        mutableStateOf(true)
+    }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            //OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            //Greetings()
+        }
+    }
+}
